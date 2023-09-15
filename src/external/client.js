008fd437cb94a7940e1895e344213e2ac6f410a5
ignacio lugones <ignacio.lugones@wibson.org>
2023-09-15 18:17:18 -0300

    add new endpoints

---
+++ src/external/client.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const getQueryParamsAsString = (query) => Object.entries(query)
+  .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
+  .join('&');
+
+class Client {
+  axiosClient;
+
+  constructor() {
+    this.axiosClient = axios.create();
+  }
+
+  async get(url, headers, queryParams) {
+    const fullUrl = `${url}?${getQueryParamsAsString(queryParams)}`
+    try {
+      const {data} = await this.axiosClient.get(fullUrl, {headers: headers});
+      return data;
+    } catch (e) {
+      const msgError = `failed to fetch, error is: ${e.message}, url was: ${fullUrl}`;
+      throw new Error(msgError)
+    }
+  }
+
+}
+
+export default Client;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,31 @@
 import Router from 'express';
+import {XMLParser} from 'fast-xml-parser';
+import {decode} from 'metar-decoder';
+import Client from '../external/client.js';
+import run from "../middleware/run.js";
 
+const parser = new XMLParser();
+
+const client = new Client();
 const router = Router();
-router.get('/ping', (res, req) => req.send('pong'));
+router.get('/ping', (req, res) => req.send('pong'));
+router.get('/metar', run(async (req) => {
+  const { station } = req.query;
+  const query = {
+    datasource: 'metars',
+    requestType: 'retrieve',
+    format: 'xml',
+    stationString: station,
+    hoursBeforeNow: 1,
+  }
+  const data = await client.get('https://www.aviationweather.gov/adds/dataserver_current/httpparam', {}, query)
+  const parsed = parser.parse(data);
+  if (!parsed.data_source) {
+    const e = new Error('invalid station used');
+    e.status = 400;
+    throw e;
+  }
+  return decode(parsed.response.data.METAR.raw_text);
+}));
 
 export default router;
