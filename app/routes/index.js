05ef5ce8106cc0adfc059c3c6d7a300676c196de
ignacio lugones <ignacio.lugones@wibson.org>
2023-09-17 20:43:44 -0300

    change metar

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,31 +1,7 @@
 import Router from 'express';
-import {XMLParser} from 'fast-xml-parser';
-import {decode} from 'metar-decoder';
-import Client from '../external/client.js';
-import run from "../middleware/run.js";
-
-const parser = new XMLParser();
-
-const client = new Client();
+import metarApi from './metar.js'
 const router = Router();
 router.get('/ping', (req, res) => res.send('pong'));
-router.get('/metar', run(async (req) => {
-  const { station } = req.query;
-  const query = {
-    datasource: 'metars',
-    requestType: 'retrieve',
-    format: 'xml',
-    stationString: station,
-    hoursBeforeNow: 1,
-  }
-  const data = await client.get('https://www.aviationweather.gov/adds/dataserver_current/httpparam', {}, query)
-  const parsed = parser.parse(data);
-  if (!parsed.data_source) {
-    const e = new Error('invalid station used');
-    e.status = 400;
-    throw e;
-  }
-  return decode(parsed.response.data.METAR.raw_text);
-}));
+router.use('/metar', metarApi)
 
 export default router;
---
+++ app/routes/metar.js
@@ -0,0 +1,30 @@
+import run from "../middleware/run.js";
+import {decode} from "metar-decoder";
+import Router from "express";
+import {XMLParser} from "fast-xml-parser";
+import Client from "../external/client.js";
+
+const parser = new XMLParser();
+const client = new Client();
+const router = Router();
+
+router.get('/', run(async (req) => {
+  const { station } = req.query;
+  const query = {
+    datasource: 'metars',
+    requestType: 'retrieve',
+    format: 'xml',
+    stationString: station,
+    hoursBeforeNow: 1,
+  }
+  const data = await client.get('https://www.aviationweather.gov/adds/dataserver_current/httpparam', {}, query)
+  const parsed = parser.parse(data);
+  if (!parsed.response.data) {
+    const e = new Error('invalid station used');
+    e.status = 400;
+    throw e;
+  }
+  return decode(parsed.response.data.METAR.raw_text);
+}));
+
+export default router;
