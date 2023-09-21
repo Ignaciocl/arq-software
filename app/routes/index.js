1914f67f1d0b468809bd1f14bf4fd1329ac864ff
Franco <fbarreneche@fi.uba.ar>
2023-09-20 22:24:19 -0300

    Add cache middleware use example

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,11 +2,14 @@ import Router from 'express';
 import metarApi from './metar.js';
 import quoteApi from './quote.js';
 import spaceflightNewsApi from './spaceflightNews.js';
+import {readCache} from '../middleware/cache.js'
 
 const router = Router();
+
 router.get('/ping', (req, res) => res.send('pong'));
 router.use('/metar', metarApi);
 router.use('/quote', quoteApi);
 router.use('/spaceflight_news', spaceflightNewsApi);
+router.use('/increment', readCache, increment)
 
 export default router;
