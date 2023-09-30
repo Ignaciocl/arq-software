263e6b212a1ada5795ed8fef2650e436a27ad3df
ignacio lugones <ignacio.lugones@wibson.org>
2023-09-30 19:04:56 -0300

    annoy franco x2

--- app/middleware/run.js
+++ app/middleware/run.js
@@ -1,18 +1,14 @@
 import { nanoid } from 'nanoid';
-import { writeCache } from './cache.js';
-import {finishTracking, log} from "./tracking.js";
+import {finishTracking, log, startTracking} from "./tracking.js";
 
 const id = nanoid();
-const EXPIRATION_TIME = 5;
 
 const run = (handler) => {
   return async (req, res, next) => {
+    startTracking(req);
     const path = req.originalUrl.split('?')[0];
     try {
       const [response, expTime] = await handler(req, res, next);
-      if (expTime !== 0) {
-        await writeCache(req.originalUrl, JSON.stringify(response), expTime || EXPIRATION_TIME);
-      }
       res.setHeader('X-API-Id', id)
       res.status(!!response ? 200 : 204).json(response);
       log(req, `success.${path.split('/')[1]}.200`)
