c1d042534307fa3b1823f81b47c78e006b6d8506
ignacio lugones <ignacio.lugones@wibson.org>
2023-10-03 18:30:29 -0300

    add cache to branch

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,12 +3,14 @@ import metarApi from './metar.js';
 import quoteApi from './quote.js';
 import spaceflightNewsApi from './spaceflightNews.js';
 import increment from "./increment.js";
+import {readCache} from "../middleware/cache.js";
 
 const router = Router();
 
-router.get('/ping', (req, res) => res.send('pong'));
-router.use('/metar', metarApi);
 router.use('/quote', quoteApi);
+router.get('/ping', (req, res) => res.send('pong'));
+router.use(readCache);
+router.use('/metar', metarApi);
 router.use('/spaceflight_news', spaceflightNewsApi);
 router.use('/increment', increment);
 
