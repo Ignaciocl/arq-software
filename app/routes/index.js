87b45e9a469063c15046c78c9bdeff0f203c0951
ignacio lugones <ignacio.lugones@wibson.org>
2023-10-05 15:07:45 -0300

    add ping to timing

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,11 +4,14 @@ import quoteApi from './quote.js';
 import spaceflightNewsApi from './spaceflightNews.js';
 import increment from "./increment.js";
 import {readCache} from "../middleware/cache.js";
+import run from "../middleware/run.js";
 
 const router = Router();
 
 router.use('/quote', quoteApi);
-router.get('/ping', (req, res) => res.send('pong'));
+router.get('/ping', run((req, res, next) => {
+  return [{ping: 'pong'}, 0];
+}));
 router.use(readCache);
 router.use('/metar', metarApi);
 router.use('/spaceflight_news', spaceflightNewsApi);
